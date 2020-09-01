8d7cc310a1d485c00500790bb8d106db55a74268
Oskar Šerelis <oskaras34@gmail.com>
2020-09-01 04:05:22 -0700

    Added hamburger menu for mobile and updated animations]

--- js/index.js
+++ js/index.js
@@ -1,67 +1,24 @@
-const debounce = (fn) => {
-    let frame;
-    return (...params) => {
-      if (frame) { 
-        cancelAnimationFrame(frame);
-      }
-      frame = requestAnimationFrame(() => {
-        fn(...params);
-      });
-   } 
-};
-const storeScroll = () => {
-    document.documentElement.dataset.scroll = window.scrollY;
-    let screenWidth = document.querySelector('html').offsetWidth;
-    if (screenWidth <= 640) {
-        if (document.documentElement.dataset.scroll > 270) {
-            document.querySelectorAll('nav a').forEach( a => {
-                a.style.color = "#3F7FBF";
-            });
-            $(document).ready(function(){
-                $("nav a").hover(function(){
-                  $(this).css("border-bottom", "2px solid #3F7FBF");
-                  }, function(){
-                  $(this).css("border-bottom", "none");
-                });
-              });
-        } else {
-            document.querySelectorAll('nav a').forEach( a => {
-                a.style.color = "#fff";
-            });
-            $(document).ready(function(){
-                $("nav a").hover(function(){
-                  $(this).css("border-bottom", "2px solid #fff");
-                  }, function(){
-                    $(this).css("border-bottom", "none");
-                  });
-              });
-        }
-    } else {
-        if (document.documentElement.dataset.scroll >= 550) {
-            document.querySelectorAll("nav a").forEach( a => {
-                a.style.color = "#3F7FBF";
-            });
-            $(document).ready(function(){
-                $("nav a").hover(function(){
-                  $(this).css("border-bottom", "2px solid #3F7FBF");
-                  }, function(){
-                    $(this).css("border-bottom", "none");
-                  });
-              });
-        } else {
-            document.querySelectorAll('nav a').forEach( a => {
-                a.style.color = "#fff";
-            });
-            $(document).ready(function(){
-                $("nav a").hover(function(){
-                  $(this).css("border-bottom", "2px solid #fff");
-                  }, function(){
-                    $(this).css("border-bottom", "none");
-                  });
-              });
-        }
-    }
-}
-document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+// Hamburger
+$('document').ready(function () {
+  var trigger = $('#hamburger'),
+      isClosed = true;
 
-storeScroll();
+  trigger.click(function () {
+    burgerTime();
+  });
+
+  function burgerTime() {
+    if (isClosed == true) {
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      isClosed = false;
+      document.querySelector('nav').style.display = 'none';
+    } else {
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      isClosed = true;
+      document.querySelector('nav').style.display = 'block';
+    }
+  }
+
+});
